1bec726e8d34aa209fe004f560450b9c9e60c612
Serhii Mamedov <bboyvivus@gmail.com>
2020-04-10 17:54:38 +0300

    implemented basic functionality

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-require('dotenv').config();
+const pm2 = require('pm2');
 const io = require('@pm2/io');
+
+let lastTimeChecked = Date.now();
+
+const getProcesses = () => {
+  return new Promise((resolve, reject) => {
+    pm2.connect(err1 => {
+      if (err1) reject(err1);
+      pm2.list((err2, resp) => {
+        if (err2) reject(err2);
+        resolve(resp);
+      })
+    })
+  })
+}
+
+const getProcess = async id_or_name => {
+  const processes = await getProcesses();
+  switch (typeof id_or_name) {
+    case 'number':
+      return processes.filter(e => e.pm_id === id_or_name)[0];
+    case 'string':
+      return processes.filter(e => e.name === id_or_name)[0];
+  }
+}
+
+const getSanitizedProсessData = prs => {
+  const obj = {
+    id: prs.pm_id,
+    name: prs.name,
+    pid: prs.pid,
+    created_at: prs.pm2_env.created_at,
+    uptime: Date.now() - prs.pm2_env.created_at,
+    last_time_checked: ~~((Date.now() - lastTimeChecked) / 1000)
+  }
+  lastTimeChecked = Date.now();
+  return obj;
+}
+
+const successCounter = io.counter({ name: 'successCounter' });
+const errorCounter = io.counter({ name: 'errorCounter' });
+
+/**
+ * provide id (as number) or name (as string) of process
+ * @param {number | string} id_or_name
+ */
+module.exports = async id_or_name => {
+  const process = await getProcess(id_or_name);
+  if (!process) throw new Error('no such pm2 process found!');
+  return {
+    data: process,
+    sanitizedData: getSanitizedProсessData(process),
+    successCounter,
+    errorCounter,
+    async update() {
+      this.data = await getProcess(this.sanitizedData.id);
+      this.sanitizedData = getSanitizedProсessData(this.data);
+    }
+  }
+}
