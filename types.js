e10b3e5c55ef41172dbdca669a9fe8ffc6375399
Serhii Mamedov <bboyvivus@gmail.com>
2020-05-18 17:25:29 +0300

    fixed jsdoc

--- types.js
+++ types.js
@@ -1,7 +1,7 @@
 /**
  * @typedef Config
  * @type {object}
- * @property {Array<{name: string, group: string, unit: string}>} [counters] - optional Array of counter names
+ * @property {Array<{name: string, group: string}>} [counters] - optional Array of counter names
  * @property {Array<{name: string, value: any, group: string, unit: string}>} [fields] - optional Array of field names.
  * @property {Array<{name: string, group: string, unit: string}>} [histogrms] - optional Array of histograms
  */
